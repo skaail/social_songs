38500fb71643902b054855428d5fce4e2300b13e
Bruno PC <blgianini@gmail.com>
2023-12-04 17:16:04 -0300

    Estrutura inicial servidor

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+    console.log(`Server rodando na porta ${PORT}`)
+})
+
+module.exports = app
