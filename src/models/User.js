496e552161bbfe06d3bc65d1e1dbad0180a94326
Bruno PC <blgianini@gmail.com>
2023-12-05 15:30:26 -0300

    auto increment user id

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,10 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const Counter = require('./counterSchema')
 
 const userSchema = new mongoose.Schema({
+  _id: { type: Number, unique: true },
+
   name: {
     type: String,
     required: true
@@ -20,6 +23,29 @@ const userSchema = new mongoose.Schema({
 
   notas: [{type: mongoose.Schema.Types.ObjectId, ref: "Notas"}]
 })
+
+userSchema.pre('save', function (next) {
+  const user = this;
+
+  // Check if the document is new, if yes, get the next available ID from the counter
+  if (user.isNew) {
+    Counter.findByIdAndUpdate(
+      { _id: 'userId' }, // Use a unique identifier for the counter
+      { $inc: { sequence_value: 1 } },
+      { new: true, upsert: true }
+    )
+      .then((counter) => {
+        user._id = counter.sequence_value;
+        next();
+      })
+      .catch((error) => {
+        return next(error);
+      });
+  } else {
+    // If the document is not new, proceed without incrementing the counter
+    next();
+  }
+});
 
 userSchema.pre('save', async function (next) {
   const user = this
